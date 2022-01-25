6adcd8a8902afcfee5c1603022c45aad3e88a3a5
Matt <i.w.b.s.bilm@gmail.com>
2022-01-25 15:31:06 -0800

    env created

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-import express from "express";
-import  mongoose  from "mongoose";
-import bodyParser from "body-parser";
-import path from 'path';
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const password = process.env.PASSWORD;
+const adminName = process.env.ADMIN_NAME;
+const connection_url = `mongodb+srv://${adminName}:${password}@cluster0.14vum.mongodb.net/userDB?retryWrites=true&w=majority`
 
-const dirname = `/Users/MattH/Desktop/Projects/web/building-server`
+mongoose.connect(connection_url, {useNewUrlParser: true, useUnifiedTopology: true})
 const app = express()
 const port = process.env.PORT || 1020;
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('/src/'));
+app.use(express.static('src/'));
 
-app.get('/',(req,res)=>{res.status(200).sendFile(`${dirname}/src/index.html`)})
-app.get('/about',(req,res)=>{res.status(200).send('<h1>Matt Hamilton, 26 yrs old</h1>')})
+app.get('/',(req,res)=>{res.status(200).sendFile(`${dirname} index.html`)});
+
+app.get('/about',(req,res)=>{res.status(200).send('<h1>Matt Hamilton, 26 yrs old</h1>')});
 
 app.listen(port,()=>{console.log(`the port runs on ${port}`);})
